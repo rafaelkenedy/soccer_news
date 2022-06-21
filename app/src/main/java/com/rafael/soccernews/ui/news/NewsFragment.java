a0f16575fabd5f0396dac239f10bbb13d56ca42c
rafaelkenedy <k.rafaelalves@gmail.com>
2022-06-21 08:43:17 -0300

    Criando adapter e testando RecyclerView!

--- app/src/main/java/com/rafael/soccernews/ui/news/NewsFragment.java
+++ app/src/main/java/com/rafael/soccernews/ui/news/NewsFragment.java
@@ -9,8 +9,10 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.rafael.soccernews.databinding.FragmentNewsBinding;
+import com.rafael.soccernews.ui.adapter.NewsAdapter;
 
 public class NewsFragment extends Fragment {
 
@@ -24,9 +26,9 @@ public class NewsFragment extends Fragment {
         binding = FragmentNewsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-
+        binding.rvNews.setLayoutManager(new LinearLayoutManager(getContext()));
         newsViewModel.getNews().observe(getViewLifecycleOwner(), news -> {
-
+            binding.rvNews.setAdapter(new NewsAdapter(news));
         });
         return root;
     }
